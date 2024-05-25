289ff75f1b7c02be789124c9fd8c77c321e24f5b
shiba486 <shivaaydas486@gmail.com>
2024-05-25 08:16:19 +0600

    security middleware require and implemantation

--- app.js
+++ app.js
@@ -1,11 +1,71 @@
-import express from "express"
-
+import express from "express";
 const app = express()
 
+
+//security libray require
+import path from "path"
+import { fileURLToPath } from "url";
+// configuration
+const __filename= fileURLToPath(import.meta.url)
+const __dirname= path.dirname(__filename)
+
+
+
+import cors from "cors"
+import helmet from "helmet"
+import hpp from "hpp"
+import cookieParser from "cookie-parser"
+import mongoSanitize from "express-mongo-sanitize";
+import { rateLimit } from 'express-rate-limit'
+
+
+// CORS INITIAL
+app.use(cors(
+    {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+        methods:["get","post","put","delete"],
+        
+    }
+));
+
+// SECURITY IMPLEMENTATION 
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(hpp());
+app.use(express.json({limit: "18kb"}));
+app.use(express.urlencoded({extended: true, limit: "18kb"}));
+app.use(cookieParser());
+
+const limiter = rateLimit({windowMs: 15 * 60 * 1000, limit: 100})
+app.use(limiter)
+app.use(mongoSanitize());
+
+// ROUTES
+
+
+
+
+//home route
 app.get("/",(req,res)=>{
-    res.status(200).json({"message": "WELCOME TO THIS E-COMMERCE WEB API...👻"})
+    res.json({
+        message: "WELCOME TO THIS E-COMMERCE WEB API...👻"
+    })
 })
 
+app.use(express.static("client/dist"))
+
+// //Add React Front End Routing
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
+// })
+
+//home route
+app.get("*",(req,res)=>{
+    res.status(404).json({
+        status: 404,
+        message: "PAGE NOT FOUND"
+    })
+})
 
 
 export {app}
